d3c9b2c13244deb2af5bb274d68f3e671e21e8ca
Estefanía Aguas <estefaniaaguas@gmail.com>
2024-08-15 19:07:13 -0500

    chore: se crea estructura inicial del proyecto

---
+++ parcial-fe3/src/App.jsx
@@ -0,0 +1,15 @@
+import Card from "./components/Card"
+import Form from "./components/Form"
+
+function App() {
+
+  return (
+    <>
+    <h1></h1>
+    <Form />
+     <Card />
+    </>
+  )
+}
+
+export default App
---
+++ parcial-fe3/src/components/Card.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+const Card = () => {
+    return <div>Soy una tarjeta</div>;
+};
+
+export default Card;
